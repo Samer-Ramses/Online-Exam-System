b27e28281b18d7d83d5830256865b7785bd32d62
Samer Ramses <sobertgueni@gmail.com>
2023-11-30 20:51:26 +0200

    Add project files.

---
+++ Online Exam System/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Online_Exam_System.Models;
+
+namespace Online_Exam_System.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Option> Options { get; set; }
+        public DbSet<ExamAttempt> ExamAttempts { get; set; }
+        public DbSet<UserAnswer> UserAnswers { get; set; }
+        public DbSet<ExamResult> ExamResults { get; set; }
+    }
+}
