485949444fb0d261689e9c1a47ece735529dfbe2
Mahmoud Saber <mahmoud.saber203020@gmail.com>
2023-12-05 22:43:12 +0200

    adding admincontroller and adminIndex

---
+++ Online Exam System/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Online_Exam_System.Controllers
+{
+	[Authorize(Roles = "Admin")]
+	public class AdminController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult CreateUser()
+		{
+			return View();
+		}
+
+		public IActionResult AllUsers()
+		{
+			return View();
+		}
+
+		public IActionResult Settings()
+		{
+			return View();
+		}
+	}
+}
